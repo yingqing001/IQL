daabe081a3adec18ecf05c601966933267742555
Jiaxin Xiao <95730453+cookie519@users.noreply.github.com>
2024-04-20 19:52:10 -0400

    Update learner.py

--- learner.py
+++ learner.py
@@ -17,7 +17,7 @@ import time
 
 
 def target_update(critic: Model, target_critic: Model, tau: float) -> Model:
-    new_target_params = jax.tree_multimap(
+    new_target_params = jax.tree.map(
         lambda p, tp: p * tau + tp * (1 - tau), critic.params,
         target_critic.params)
 
